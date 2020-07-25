4e0b924deeb5dfc0f3ef06456f7b41f11c8c6ec2
fuji <fujitash4@gmail.com>
2020-07-25 12:56:45 +0000

    Updates

---
+++ precache-manifest.0ef9eaa038f915f3ed653febd07bde64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efe3f64aad73445fa2673e6cc2691ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85fdc4da84e598e8950b",
+    "url": "./static/css/2.4fc9af12.chunk.css"
+  },
+  {
+    "revision": "e391f16118d0a3426e9f",
+    "url": "./static/css/main.39da44df.chunk.css"
+  },
+  {
+    "revision": "85fdc4da84e598e8950b",
+    "url": "./static/js/2.d5d04416.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.d5d04416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e391f16118d0a3426e9f",
+    "url": "./static/js/main.24dc025b.chunk.js"
+  },
+  {
+    "revision": "45f1a1b3c39a2f9cd2ee",
+    "url": "./static/js/runtime-main.0a81721e.js"
+  }
+]);
